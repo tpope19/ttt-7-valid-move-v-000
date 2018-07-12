f5eecd2a1f3b7602eea8de5e27986a78663e1c29
Thomas Pope <tpope0928@gmail.com>
2018-07-12 02:58:27 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -12,6 +12,6 @@ def position_taken?(board, index)
      if (board[index] == " ") || (board[index] == "") || (board[index] == nil)
        return false
      else
-       return true   #board[index] is not " " or "" or nil because there's an "X" or "O"
+       return true
      end
    end
